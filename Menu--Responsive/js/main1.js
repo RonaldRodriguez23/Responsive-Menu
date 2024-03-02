efd664800cd43f773994d61eac64ff29b57fab8a
Ronald Rodriguez <ronald.fjimenez89@gmail.com>
2024-03-02 09:36:10 -0500

    agrando pag 2

---
+++ Menu--Responsive/js/main1.js
@@ -0,0 +1,10 @@
+const letraR = document.querySelector('.letra-r');
+
+letraR.addEventListener('mouseover', () => {
+letraR.classList.add('letra-r-hover');
+});
+
+letraR.addEventListener('mouseout', () => {
+letraR.classList.remove('letra-r-hover');
+});
+
